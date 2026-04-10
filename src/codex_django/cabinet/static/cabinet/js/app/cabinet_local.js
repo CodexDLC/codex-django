3c390f7f650cc94d100e381cab19106a4409f482
CodexDLC <primecodex@gmail.com>
2026-04-10 11:34:04 +0200

    chore: apply pre-commit whitespace fix

--- src/codex_django/cabinet/static/cabinet/js/app/cabinet_local.js
+++ src/codex_django/cabinet/static/cabinet/js/app/cabinet_local.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Local project JS overrides.
  * In your local project, configure compiler_config.json to output to "app/cabinet_local.js".
  * This placeholder prevents ManifestStaticFilesStorage errors in projects that don't compile JS.
